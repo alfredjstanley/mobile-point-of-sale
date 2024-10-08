48b26f8c3b23094ec988bbb5d24336bbe0b49b86
stanlymathai <stanly@math.ai>
2024-10-08 15:51:19 +0530

    add application middlewares (cors, limiter, helmet, etc..)

---
+++ src/middlewares/app.middleware.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimiter = require("express-rate-limit");
+
+const RATE_LIMIT_WINDOW = 15 * 60 * 1000; // 15 minutes.
+const RATE_LIMIT_MAX_REQUESTS = 250; // Maximum number of requests per window.
+
+const limiterConfig = rateLimiter({
+  windowMs: RATE_LIMIT_WINDOW,
+  max: RATE_LIMIT_MAX_REQUESTS,
+  message: {
+    error: "Apparently, you're requesting too much. please try again later.",
+  },
+  headers: false, // Disable X-RateLimit headers
+});
