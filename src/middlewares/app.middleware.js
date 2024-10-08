ce62cfb898bb6c23309f73952013dd006b138f3e
stanlymathai <stanly@math.ai>
2024-10-08 20:28:45 +0530

    add app middleware fn's

--- src/middlewares/app.middleware.js
+++ src/middlewares/app.middleware.js
@@ -14,3 +14,12 @@ const limiterConfig = rateLimiter({
   },
   headers: false, // Disable X-RateLimit headers
 });
+
+module.exports = (app) => {
+  app.disable("x-powered-by");
+  app.use(helmet());
+  app.use(cors());
+  app.use(limiterConfig);
+  app.use(express.json({ limit: "5mb" }));
+  app.use(express.urlencoded({ extended: false }));
+};
