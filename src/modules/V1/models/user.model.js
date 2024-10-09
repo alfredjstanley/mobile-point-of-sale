1c4b7cbdae269ad4c11df3bf0a6982e43b3c6cf8
stanlymathai <stanly@math.ai>
2024-10-09 15:03:45 +0530

    chore; add initial user schema model

---
+++ src/modules/V1/models/user.model.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
+      trim: true,
+      maxlength: [20, "First name cannot be more than 20 characters"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required"],
+      trim: true,
+      maxlength: [20, "Last name cannot be more than 20 characters"],
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female", "non-binary", "not-specified"],
+    },
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      unique: true,
+      maxlength: [20, "username cannot be more than 25 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [
+        /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+        "Please enter a valid email",
+      ],
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["Super-Admin", "Admin", "Staff", "Normal"],
+    },
+    avatar: {
+      type: String,
+    },
+    secretOrKey: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["ACTIVE", "INACTIVE", "BLOCKED", "PENDING"],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+// index on the most used fields.
+userSchema.index({ username: 1 });
+userSchema.index({ secretOrKey: 1 });
+
+module.exports = mongoose.model("User", userSchema);
