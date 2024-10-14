aa3e0eee2e42ad7017206777805f4d7d6bf7aa82
stanlymathai <stanly@math.ai>
2024-10-14 11:23:25 +0530

    chore; beautify 'index' route message.

--- src/utils/messages.util.js
+++ src/utils/messages.util.js
@@ -1,6 +1,6 @@
 const MESSAGES = {
   WELCOME_MESSAGE:
-    "Welcome to MPOS API Server — where transactions come to life! 🌟",
+    "Welcome to MPOS API Server — where transactions come to life! 🌟\n",
 };
 
 module.exports = MESSAGES;
