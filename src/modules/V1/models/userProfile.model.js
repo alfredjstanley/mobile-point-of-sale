5dfff42f7e01dd9339896e982f847e8137bde56f
stanlymathai <stanly@math.ai>
2024-10-14 17:25:42 +0530

    add 'userProfile' schema model.

---
+++ src/modules/V1/models/userProfile.model.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const userProfileSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserAuth",
+      required: true,
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      maxlength: [25, "Username cannot be more than 25 characters"],
+    },
+    firstName: {
+      type: String,
+      trim: true,
+      maxlength: [20, "First name cannot be more than 20 characters"],
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      maxlength: [20, "Last name cannot be more than 20 characters"],
+    },
+    gender: {
+      type: String,
+      default: "not-specified",
+      enum: ["male", "female", "non-binary", "not-specified"],
+    },
+    avatar: {
+      type: String,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [
+        /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})$/,
+        "Please enter a valid email",
+      ],
+    },
+    address: {
+      street: String,
+      city: String,
+      state: String,
+      postalCode: String,
+      country: String,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+userProfileSchema.index({ email: 1 });
+userProfileSchema.index({ username: 1 });
+
+module.exports = mongoose.model("UserProfile", userProfileSchema);
