5c0a28de08dfb1c2140733f7433a46b6c349fe64
stanlymathai <stanly@math.ai>
2024-10-18 11:57:04 +0530

    chore; add verfiy user fn's

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,18 +1,18 @@
 const jwt = require("jsonwebtoken");
 const { app_key } = require("../configs/env.config/app.env");
 
-function verifyToken(req, res, next) {
+function verifyUser(req, res, next) {
   const authHeader = req.headers["authorization"];
+
   const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) return res.status(403).json({ message: "Forbidden" });
 
   jwt.verify(token, app_key, (error, decoded) => {
     if (error) return res.status(401).json({ message: "Token is invalid" });
-
-    req.user = decoded;
+    if (decoded.identiifier) req.user = decoded;
     next();
   });
 }
 
-module.exports = verifyToken;
+module.exports = verifyUser;
--- src/modules/V1/routes/resource/district.route.js
+++ src/modules/V1/routes/resource/district.route.js
@@ -1,41 +1,43 @@
 const express = require("express");
 const router = express.Router();
-const districtController = require("../../controllers/resource/district.controller");
+const handler = require("../../controllers/resource/district.controller");
+
+const verifyUser = require("../../../../middlewares/auth.middleware");
 
 /**
  * @route   GET /districts
  * @desc    Get all districts
  */
-router.get("/", districtController.getDistricts);
+router.get("/", verifyUser, handler.getDistricts);
 
 /**
  * @route   POST /districts
  * @desc    Create a new district
  */
-router.post("/", districtController.createDistrict);
+router.post("/", verifyUser, handler.createDistrict);
 
 /**
  * @route   GET /districts/state/:stateCode
  * @desc    Get all districts in a state
  */
-router.get("/state/:id", districtController.getDistrictsByStateCode);
+router.get("/state/:id", verifyUser, handler.getDistrictsByStateCode);
 
 /**
  * @route   GET /districts/:id
  * @desc    Get a district by ID
  */
-router.get("/:id", districtController.getDistrictById);
+router.get("/:id", verifyUser, handler.getDistrictById);
 
 /**
  * @route   PUT /districts/:id
  * @desc    Update a district by ID
  */
-router.put("/:id", districtController.updateDistrict);
+router.put("/:id", verifyUser, handler.updateDistrict);
 
 /**
  * @route   DELETE /districts/:id
  * @desc    Delete a district by ID
  */
-router.delete("/:id", districtController.deleteDistrict);
+router.delete("/:id", verifyUser, handler.deleteDistrict);
 
 module.exports = router;
