97a6bf542ad684507673bd4b2709e475ee058431
stanlymathai <stanly@math.ai>
2024-11-09 10:40:32 +0530

    refactor(transaction): Remove SavedOrder model from transaction index

--- src/modules/V1/models/transaction/index.js
+++ src/modules/V1/models/transaction/index.js
@@ -1,6 +1,5 @@
 const saleModel = require("./sales.model");
 const quickSaleModel = require("./quickSale.model");
-const saveOrderModel = require("./savedOrder.model");
 
 const purchaseModel = require("./purchase.model");
 const taxTransactionModel = require("./taxTransaction.model");
@@ -10,7 +9,6 @@ const accountTransactionModel = require("./accountTransaction.model");
 module.exports = {
   Sale: saleModel,
   QuickSale: quickSaleModel,
-  SavedOrder: saveOrderModel,
 
   Purchase: purchaseModel,
   TaxTransaction: taxTransactionModel,
