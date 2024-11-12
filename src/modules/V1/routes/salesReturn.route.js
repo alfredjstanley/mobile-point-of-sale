5cd73677669cf2a636bf297b8b08ee6b5d955d38
stanlymathai <stanly@math.ai>
2024-11-12 11:17:57 +0530

    refactor(salesReturn): Remove sales return routes, model, controller, and service

--- src/modules/V1/routes/salesReturn.route.js
+++
@@ -1,8 +0,0 @@
-const salesReturnRouter = require("express").Router();
-const handler = require("../controllers/salesReturn.controller");
-
-salesReturnRouter.post("/", handler.createSalesReturn);
-salesReturnRouter.get("/", handler.getAllSalesReturns);
-salesReturnRouter.get("/get", handler.getSalesReturnById);
-
-module.exports = salesReturnRouter;
